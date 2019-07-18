33912b50bb8f008bee430613f5ab679d5dd6a985
Felipe Kunzler <felipe.kunzler@sap.com>
2019-07-18 16:36:21 -0300

    Add ToC components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopHeaderComponent } from './top-header/top-header.component';
+import { HomeComponent } from './home/home.component';
+import { TocFormComponent } from './toc-form/toc-form.component';
+import { TocComponent } from './toc/toc.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopHeaderComponent
+    TopHeaderComponent,
+    HomeComponent,
+    TocFormComponent,
+    TocComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/toc-form/toc-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-toc-form',
+  templateUrl: './toc-form.component.html',
+  styleUrls: ['./toc-form.component.scss']
+})
+export class TocFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
