a782d1dce52a436102d133f9a7ac06fdf14d8c4c
Felipe Kunzler <felipe.kunzler@sap.com>
2019-07-19 17:18:30 -0300

    Update routes for github pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '**', component: HomeComponent },
 ];
 
 @NgModule({
